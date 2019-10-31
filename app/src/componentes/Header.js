0be76edfa8e4239411821c0511e4b7fc7d29b4c9
Eriksen Lezama <eriksen.lezama@gmail.com>
2019-10-31 12:36:13 +0100

    Ejercicio 4

---
+++ app/src/componentes/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <header className="App-heading App-flex">
+      <button className="pichichis">Pichichis</button>
+    </header>
+  )
+}
+
+export default Header
